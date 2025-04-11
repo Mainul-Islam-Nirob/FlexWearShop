f6ca4ffcbf3449360b6421e7f4e90bf03c1e8ff1
Mainul Islam <maainul.islam@gmail.com>
2025-04-11 11:55:46 +0600

    Trying to fix path related deployment issues

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 
-import Shop from './components/Shop.jsx';
+// import Shop from './components/Shop.jsx';
 import Login from './components/Login.jsx';
 import SignUp from './components/SignUp.jsx';
 import ErrorPage from './components/ErrorPage.jsx';
@@ -16,10 +16,10 @@ const routes = [
     element: <App />,
     errorElement: <ErrorPage />,
   },
-  {
-    path: "shop",
-    element: <Shop />,
-  },
+  // {
+  //   path: "shop",
+  //   element: <Shop />,
+  // },
   {
     path: "login",
     element: <Login />
